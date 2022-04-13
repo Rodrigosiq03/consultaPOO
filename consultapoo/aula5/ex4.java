79d878f7ae509c53c8d56712b854729c182efee3
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2022-04-13 08:37:01 -0300

    Add files via upload

--- consultapoo/aula5/ex4.java
+++ consultapoo/aula5/ex4.java
@@ -6,8 +6,22 @@ import java.util.Scanner;
 public class ex4 {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        int n1 = scan.nextInt();
-        String pinto = "nao sei vai toma no cu";
+        int n = scan.nextInt();
+        int contador = 0;
+        for (int i = 1; i <= n + 1; i = i + 1) {
+            if (n % i == 0) {
+                System.out.println(n + " é divisivel por " + i);
+                contador = contador + 1;
+            }
+        }
+        if (contador > 2) {
+            System.out.println(n + " foi divisivel " + contador + " vezes, por isso ele NAO É PRIMO");
+        } else if (contador == 2) {
+            System.out.println(n + " foi divisivel apenas " + contador + " vezes, por isso ELE É PRIMO");
+        } else {
+            System.out.println(n + " foi divisivel apenas " + contador + " vez, por isso ele NAO É  PRIMO");
+        }
+        scan.close();
     }
 
 }
